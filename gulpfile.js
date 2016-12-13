bed95b1035a98a6be1f84e18f669ccbf8b127b92
Brian Quach <13rianquach@gmail.com>
2016-12-12 19:40:03 -0800

    Chore: configure code to work with virtual env

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp');
+var del = require('del');
 
-gulp.task('deploy', function () {
-  gulp.src(['humblydesign/**/*.{html,css,js}', '!humblydesign/venv{,/**}'], { base: '.' })
+gulp.task('deploy:clean', function () {
+  return del(['/var/www/humblydesign'], { force: true }).then(paths => {
+    console.log('Deleted files and folders:\n', paths.join('\n'));
+  });
+});
+gulp.task('deploy:copy', ['deploy:clean'], function () {
+  gulp.src([
+    'dist/**/*',
+    '!dist/humblydesign/**/*.{log,pyc}',
+    '!dist/humblydesign/**/.*',
+    '!dist/venv',
+    '!dist/venv/**/*'
+  ], { base: 'dist' })
     .pipe(gulp.dest('/var/www/humblydesign'));
-  // gulp.src('humblydesign.wsgi')
-  //   .pipe(gulp.dest('/var/www/humblydesign'));
 });
 
-gulp.task('default', ['deploy']);
+gulp.task('deploy', ['deploy:copy'], function() {
+  console.log('Deploy finished');
+});
