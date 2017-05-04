f2ba1dda11677a30cf47355ff12a4c124be39fc6
Brian Quach <13rianquach@gmail.com>
2017-05-03 17:51:12 -0700

    Build: run pip installs

--- dist/humblydesign.wsgi
+++ dist/humblydesign.wsgi
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-activate_this = '/home/bquach/humblydesign/dist/venv/bin/activate_this.py'
+activate_this = '/var/www/humblydesign/venv/bin/activate_this.py'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
--- start.sh
+++ start.sh
@@ -5,4 +5,10 @@ source /$HOME/.nvm/nvm.sh
 
 #start the app
 app=$HOME/humblydesign
+
+# Clear all files and copy over updated files
 cd /var/www/humblydesign && rm -rf * && cd $app/dist && cp -R * /var/www/humblydesign
+
+# Activate virtual environment
+cd /var/www/humblydesign && source venv/bin/activate
+pip install $app/requirements.txt && deactivate
